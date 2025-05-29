595e984b12f3000317ba5b7489115e5a64da9d26
adeliptr <adeliptr@gmail.com>
2025-05-29 14:15:08 +0900

    add Colors constants

---
+++ src/lib/constants/Colors.ts
@@ -0,0 +1,40 @@
+export const Colors = {
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: {
+        light50: '#F0F0F0',
+        light100: '#E6E6E6',
+        light200: '#C6C6C6',
+        light300: '#B0B0B0',
+        med400: '#9B9B9B',
+        med500: '#868686',
+        med600: '#727272',
+        dark700: '#5E5E5E',
+        dark800: '#4B4B4B',
+        dark900: '#393939'
+    },
+    planner: {
+        light50: '#A5FFFF',
+        light100: '#83EDFA',
+        light200: '#60D7E5',
+        light300: '#38C1D0',
+        med400: '#00ACBB',
+        med500: '#0096A6',
+        med600: '#008190',
+        dark700: '#006C7B',
+        dark800: '#005865',
+        dark900: '#004450'
+    },
+    memory: {
+        light50: '#FFCFAD',
+        light100: '#FFB491',
+        light200: '#FF9976',
+        light300: '#FF7F5E',
+        med400: '#FF6747',
+        med500: '#EE4F33',
+        med600: '#D43721',
+        dark700: '#B82010',
+        dark800: '#9B0502',
+        dark900: '#7D0000'
+    }
+}
